f2af3116356c9b5550f2835d145e4804083e59e8
Veena Goud <veena.y@learnfinity.co>
2022-03-29 17:34:12 +0530

    Minor changes

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -1,76 +1,75 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { AuthContext } from '../../context/auth/AuthContext'
-import { Link, Redirect } from 'react-router-dom'
+import React, { useState, useContext, useEffect } from "react";
+import { AuthContext } from "../../context/auth/AuthContext";
+import { Link, Redirect } from "react-router-dom";
 
-function Login () {
-  const { token, login, error, isAuthenticated } = useContext(AuthContext)
+function Login() {
+  const { token, login, error, isAuthenticated } = useContext(AuthContext);
 
-  const [isLogin, setIsLogin] = useState(false)
+  const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
 
   useEffect(() => {
-    token && isAuthenticated && setIsLogin(true)
-  }, [isAuthenticated])
+    token && isAuthenticated && setIsLogin(true);
+  }, [isAuthenticated]);
 
-  const { email, password } = user
+  const { email, password } = user;
 
-  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const loginTrello = async e => {
-    e.preventDefault()
+  const loginTrello = async (e) => {
+    e.preventDefault();
     login({
       email,
-      password
-    })
-    setIsLogin(true)
+      password,
+    });
+    setIsLogin(true);
     setUser({
-      email: '',
-      password: ''
-    })
-  }
+      email: "",
+      password: "",
+    });
+  };
 
-  return (isLogin && isAuthenticated) ? (
-    <Redirect to='/home' />
-  )
-    : (
-      <main className='login-container'>
-        <form className='user-login-form' onSubmit={loginTrello}>
-          {error && <p className='login-error'>{error.message}</p>}
-          <summary className='login-text'> Login Please!</summary>
-          <input
-            type='email'
-            className='login-input'
-            placeholder='Enter Email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
-          />
-          <input
-            type='password'
-            autoComplete='new-password'
-            className='login-input'
-            placeholder='Enter Password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-          />
-          <button
-            type='submit'
-            className='login-button'
-          >
-              Login
-          </button>
-        </form>
-     
-        <footer className='footer-text'>
-            Don't have an account. <Link to='/register' className='link-style'>Register New User</Link>
-        </footer>
-      </main>
-    )
+  return isLogin && isAuthenticated ? (
+    <Redirect to="/home" />
+  ) : (
+    <main className="login-container">
+      <form className="user-login-form" onSubmit={loginTrello}>
+        {error && <p className="login-error">{error.message}</p>}
+        <summary className="login-text"> Login Please 123!</summary>
+        <input
+          type="email"
+          className="login-input"
+          placeholder="Enter Email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="password"
+          autoComplete="new-password"
+          className="login-input"
+          placeholder="Enter Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+
+      <footer className="footer-text">
+        Don't have an account.{" "}
+        <Link to="/register" className="link-style">
+          Register New User
+        </Link>
+      </footer>
+    </main>
+  );
 }
-export default Login
+export default Login;
--- client/src/components/User/Login.js
+++ client/src/components/User/Login.js
@@ -1,75 +1,71 @@
-import React, { useState, useContext, useEffect } from 'react'
-import { AuthContext } from '../../context/auth/AuthContext'
-import { Link, Redirect } from 'react-router-dom'
+import React, { useState, useContext, useEffect } from "react";
+import { AuthContext } from "../../context/auth/AuthContext";
+import { Link, Redirect } from "react-router-dom";
 
-function Login () {
-  const { token, login, error, isAuthenticated } = useContext(AuthContext)
+function Login() {
+  const { token, login, error, isAuthenticated } = useContext(AuthContext);
 
-  const [isLogin, setIsLogin] = useState(false)
+  const [isLogin, setIsLogin] = useState(false);
   const [user, setUser] = useState({
-    email: '',
-    password: ''
-  })
+    email: "",
+    password: "",
+  });
 
-  const { email, password } = user
+  const { email, password } = user;
 
-  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
 
-  const loginDunzo = async e => {
-    e.preventDefault()
+  const loginDunzo = async (e) => {
+    e.preventDefault();
     login({
       email,
-      password
-    })
-    setIsLogin(true)
+      password,
+    });
+    setIsLogin(true);
     setUser({
-      email: '',
-      password: ''
-    })
-  }
+      email: "",
+      password: "",
+    });
+  };
 
   useEffect(() => {
-    token && setIsLogin(true)
-  }, [])
-  
-  return (isLogin && isAuthenticated) ? (
-    <Redirect to='/home' />
-  )
-    : (
-      <div className='login-container'>
-        <form className='user-login-form' onSubmit={loginDunzo}>
-          {error && <h3 className='login-error'>{error.message}</h3>}
-          <p className='login-text'> Login Please!</p>
-          <input
-            type='text'
-            className='login-input'
-            placeholder='Enter Email'
-            name='email'
-            value={email}
-            onChange={onChange}
-            required
-          />
-          <input
-            type='password'
-            autoComplete='new-password'
-            className='login-input'
-            placeholder='Enter Password'
-            name='password'
-            value={password}
-            onChange={onChange}
-            required
-          />
-          <button
-            type='submit'
-            className='login-button'
-          >
-              Login
-          </button>
-        </form>
-        <p className='footer-text'>
-            Don't have an account. <Link to='/register'>Register New User</Link>
-        </p>
-      </div>
-    )
+    token && setIsLogin(true);
+  }, []);
+
+  return isLogin && isAuthenticated ? (
+    <Redirect to="/home" />
+  ) : (
+    <div className="login-container">
+      <form className="user-login-form" onSubmit={loginDunzo}>
+        {error && <h3 className="login-error">{error.message}</h3>}
+        <p className="login-text"> Login Please 456!</p>
+        <input
+          type="text"
+          className="login-input"
+          placeholder="Enter Email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="password"
+          autoComplete="new-password"
+          className="login-input"
+          placeholder="Enter Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+      </form>
+      <p className="footer-text">
+        Don't have an account. <Link to="/register">Register New User</Link>
+      </p>
+    </div>
+  );
 }
-export default Login
+export default Login;
