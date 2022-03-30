4ce6ff8a13b81d6b773dfe02fe5e2bd808e6bb0c
Veena Goud <veena.y@learnfinity.co>
2022-03-30 13:29:51 +0530

    Add. Heroku changes

--- server/app.js
+++ server/app.js
@@ -1,69 +1,76 @@
-const express = require('express')
-const app = express()
-const routes = require('./util/routes')
-const cors = require('cors')
-require('dotenv').config()
-const port = process.env.PORT || 2809
+const express = require("express");
+const app = express();
+const routes = require("./util/routes");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
+const port = process.env.PORT || 2809;
 
-app.use(cors())
-app.use(express.json())
-app.use('/dunzo', routes)
+app.use(cors());
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  // serve static content
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+console.log("CHECK:", __dirname, path.join(__dirname, "../client/build"));
+app.use("/dunzo", routes);
 
 // sockets
-const http = require('http')
-const server = http.createServer(app)
+const http = require("http");
+const server = http.createServer(app);
 // const io = require('socket.io')(server)
 
-const io = require('socket.io')(server, {
-  
+const io = require("socket.io")(server, {
   handlePreflightRequest: (req, res) => {
-      const headers = {
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true
-      }
-      res.writeHead(200, headers)
-      res.end()
-  }})
+    const headers = {
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    };
+    res.writeHead(200, headers);
+    res.end();
+  },
+});
 
 server.listen(port, () => {
-  console.log('App running on :', port)
-})
+  console.log("App running on :", port);
+});
 
-io.sockets.on('connection', socket => {
-  socket.on('new-dp-user', name => {
-    socket.broadcast.emit('dp-connected', name)
+io.sockets.on("connection", (socket) => {
+  socket.on("new-dp-user", (name) => {
+    socket.broadcast.emit("dp-connected", name);
     const myFunc = (flag) => {
-      socket.broadcast.emit('outlet-accepted-order', flag)
-    }
+      socket.broadcast.emit("outlet-accepted-order", flag);
+    };
     setTimeout(myFunc, 2000, true);
-  })
-  
-  socket.on('items-ordered', address => {
-    socket.broadcast.emit('dp-start-delivery', address)
-  })
+  });
 
-  socket.on('delivery-partner-accepted', data => {
-    socket.broadcast.emit('dp-assigned', data)
-  })
+  socket.on("items-ordered", (address) => {
+    socket.broadcast.emit("dp-start-delivery", address);
+  });
 
-  socket.on('dp-reached-store', data => {
-    socket.broadcast.emit('dp-arrived-store', data)
-  })
+  socket.on("delivery-partner-accepted", (data) => {
+    socket.broadcast.emit("dp-assigned", data);
+  });
 
-  socket.on('dp-picked-order', data => {
-    socket.broadcast.emit('order-picked-up', data)
-  })
+  socket.on("dp-reached-store", (data) => {
+    socket.broadcast.emit("dp-arrived-store", data);
+  });
 
-  socket.on('send-address', data => {
-    socket.broadcast.emit('send-geocode-addr', data)
-  })
+  socket.on("dp-picked-order", (data) => {
+    socket.broadcast.emit("order-picked-up", data);
+  });
 
-  socket.on('new-dp-position', data => {
-    socket.broadcast.emit('locate-new-dp-pos', data)
-  })
+  socket.on("send-address", (data) => {
+    socket.broadcast.emit("send-geocode-addr", data);
+  });
 
-  socket.on('delivery-done', data => {
-    socket.broadcast.emit('delivered', data)
-  })
-})
+  socket.on("new-dp-position", (data) => {
+    socket.broadcast.emit("locate-new-dp-pos", data);
+  });
+
+  socket.on("delivery-done", (data) => {
+    socket.broadcast.emit("delivered", data);
+  });
+});
--- server/util/database.js
+++ server/util/database.js
@@ -2,13 +2,21 @@ const { Pool } = require("pg");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../../.env") });
 
-const pool = new Pool({
+const devConfig = {
   user: process.env.REACT_APP_DB_USER,
   password: process.env.REACT_APP_DB_PWD,
   host: process.env.REACT_APP_HOST,
   database: process.env.REACT_APP_DATABASE,
   port: process.env.DB_PORT,
-});
+  ssl: true,
+};
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? prodConfig : devConfig
+);
 
 const query = async (sql, params) => {
   try {
